6d5d927a09e513a67b7ef7b54fb843a99ebdc6a9
Tetiana1990 <tany9ua@gmail.com>
2024-08-16 18:13:03 +0200

    creat Lesson 3.html

---
+++ Lesson 3/main-two.js
@@ -0,0 +1,37 @@
+
+
+
+//    <!--            number1                 -->
+// for (let i =1; i<=10;i++) {
+//     document.write(`<div>element</div>`);
+// }
+//
+// <!--            number2                  -->
+// for (let i =1; i<=10;i++) {
+//     document.write(i)
+//     document.write(`<div>element</div>`);
+// }
+
+                  // number3
+
+    // let i =0;
+    // while (i<20){
+    //     document.write(`
+    //         <div>
+    //         <h1>заголовок</h1>
+    //         </div>`)
+    //     i++
+    // }
+
+              // number 4
+
+
+// let i =0;
+// while (i<20){
+//     document.write(i)
+//     document.write(`
+//             <div>
+//             <h1>заголовок</h1>
+//             </div>`)
+//     i++
+// }
